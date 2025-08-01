f543580c206db52ad4ab5cfbe24548611e42d75d
KhyzerSaleem <khizarking704@gmail.com>
2025-08-01 19:32:53 +0500

    Initial commit with new code

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: "#000", color: "#FFA500" }}>
+      <h1>BRIYANT SOLÈY SIGNO 1815 - Sit Ofisyèl</h1>
+    </div>
+  );
+}
