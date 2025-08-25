61c8c2f2bf5d15647f400d43c82a75fffbcbcc43
bssigno1815 <briyantsigno@gmail.com>
2025-08-25 03:57:49 -0400

    Create pages/mizik.js

---
+++ pages/Pages/i.js
@@ -0,0 +1,14 @@
+export default function Mizik() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      background: '#000',
+      color: '#f7921e',
+      padding: 32
+    }}>
+      <h1>Mizik</h1>
+      <p>Lis mizik yo ap vini isit la.</p>
+      <a href="/">← Retounen</a>
+    </main>
+  );
+}
