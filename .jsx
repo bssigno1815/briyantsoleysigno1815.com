5b6894038d7f84458a8579494cd0bc433718e14c
bssigno1815 <briyantsigno@gmail.com>
2025-09-09 23:54:08 -0400

    Update .jsx

--- .jsx
+++ .jsx
@@ -1,4 +1,4 @@
-<img src="/bss-logo.png" alt="BSS Logo" />export default function NotFound() {
+<img src="/bss-logo.png" alt="BSS Logo" /><img src="/bss-logo.png" alt="BSS Logo" />export default function NotFound() {
   return (
     <main style={{minHeight:'100vh',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center',background:'#000',color:'#ffa500',textAlign:'center',padding:'24px'}}>
       <h1 style={{fontSize:'64px',margin:0}}>404</h1>
