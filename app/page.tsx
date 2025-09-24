f604c64aad824d432282a43c8ed6fe896f31aef1
bssigno1815 <briyantsigno@gmail.com>
2025-09-24 19:10:06 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>BRIYANT SOLÈY SIGNO 1815</h1>
+      <p>Nou toujou kléré ✨</p>
+    </main>
+  );
+}
