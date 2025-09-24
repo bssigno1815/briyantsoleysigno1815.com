dfb8e5354ee9add5136fa404fa70c99a8acd76ef
bssigno1815 <briyantsigno@gmail.com>
2025-09-23 20:04:33 -0400

    Create pages/index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div style={{minHeight:'100vh',display:'grid',placeItems:'center',background:'#000',color:'#f97316'}}>
+      <h1>BRIYANT SOLEY SIGNO 1815</h1>
+    </div>
+  );
+}
